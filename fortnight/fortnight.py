e5c717cf6f9651756764ae6e0f4c3fcbe5f47079
Uwe Girlich <Uwe.Girlich@gmail.com>
2022-03-31 21:39:06 +0200

    generate one multipage tex file

--- fortnight/fortnight.py
+++ fortnight/fortnight.py
@@ -4,15 +4,19 @@ import datetime
 import locale
 import string
 
-text_start = r"""\documentclass[a4paper]{article}
+template_document = r"""\documentclass[a4paper]{article}
 \usepackage[utf8]{inputenc}
 \usepackage{longtable}
+\pagestyle{empty}
 \textwidth20.00cm
 \textheight27.5cm
 \oddsidemargin-2.0cm
-\topmargin-2.5cm
+\topmargin-2.8cm
 \begin{document}
-\begin{longtable}{|p{1.8cm}|p{1.8cm}|p{1.8cm}|p{1.8cm}|p{1.8cm}|p{7.3cm}|}
+$tables\end{document}
+"""
+
+template_table = r"""\begin{longtable}{|p{1.8cm}|p{1.8cm}|p{1.8cm}|p{1.8cm}|p{1.8cm}|p{7.3cm}|}
 \hline
 Datum &
 Spaziergang &
@@ -21,41 +25,50 @@ Radfahren &
 Schwimmen &
 Kommentar \\
 \hline
+$lines\end{longtable}
+\newpage
 """
 
-text_line = r"""\parbox[t]{2.3cm}{$bf$date\vspace{1.16cm}}&&&&&\\
+template_line = r"""\parbox[t]{2.3cm}{$bf$date\vspace{1.16cm}}&&&&&\\
 \hline
 """
 
-text_end = r"""\endhead
-\end{longtable}
-\end{document}
-"""
-
-def onepage(start):
-    locale.setlocale(locale.LC_ALL, "de_DE")
+def format_table(start):
     delta = datetime.timedelta(days=1)
     middle = start+delta*7
     end = start+delta*14
     d = start
-    f = open(start.strftime("page-%Y-%m-%d.tex"), "w")
-    f.write(text_start)
-    t = string.Template(text_line)
+    text_lines = ""
     while d<end:
         if d.weekday()>=5:
           bf="\\bf{}"
         else:
           bf=""
-        f.write(t.substitute(date=d.strftime("%A\\\\%d.%m.%Y"),bf=bf))
+        text_lines += string.Template(template_line).substitute(date=d.strftime("%A\\\\%d.%m.%Y"),bf=bf)
         d += delta
         if d == middle:
-          f.write("\hline")
-    f.write(text_end)
-    f.close()
+            text_lines += "\hline"
+    text_table = string.Template(template_table).substitute(lines=text_lines)
+    return text_table
+
+def format_document(start, pages):
+    delta = datetime.timedelta(days=1)
+    text_tables = ""
+    for page in range(pages):
+        d = start + delta * 14 * page
+        text_tables += format_table(d)
+    text_document = string.Template(template_document).substitute(tables=text_tables)
+    return text_document
 
 def main():
-    onepage(datetime.date(2022,3,16))
-    onepage(datetime.date(2022,3,30))
+    start = datetime.date(2022,3,16)
+    pages = 4
+
+    locale.setlocale(locale.LC_ALL, "de_DE")
+    text = format_document(start, pages)
+    f = open(start.strftime("fortnight-%Y%m%d.tex"),"w")
+    f.write(text)
+    f.close()
 
 if __name__ == '__main__':
     main()
